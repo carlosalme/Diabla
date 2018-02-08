5a1fed37ea3b251c6e417b4113575afeea1d07d6
carlosalme <ce.almeida17@gmail.com>
2018-02-08 17:47:37 -0600

    final clase jueves

--- app/src/main/java/diabla/org/diabla/MainActivity.java
+++ app/src/main/java/diabla/org/diabla/MainActivity.java
@@ -2,6 +2,8 @@ package diabla.org.diabla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.enviar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //dentro de la clase interna anonima
+                Toast.makeText(getApplicationContext(),"tu mensaje se envia",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
